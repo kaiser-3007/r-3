8629e307b50f239a88c9528e9335814acd555245
Siddharth Pokharna <siddharth@kubric.io>
2020-05-22 01:42:21 +0530

    Removing unwanted code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SimpleCubeRotation from './SimpleCubeRotation';
-import ControlsOrbit from './ControlsOrbit';
 import styles from './styles.scss';
 import ThreeScene from './libs/self/Scene';
 
@@ -9,9 +7,6 @@ const App = () => (
   <div>
     <h1 className={styles.heading}>React - ThreeJS Experiments</h1>
     <div className={styles.examples}>
-      {/* <SimpleCubeRotation /> */}
-      {/* <ControlsOrbit /> */}
-
       <ThreeScene
         containerId="basic-scene"
         dimension={{ width: 300, height: 300 }} />

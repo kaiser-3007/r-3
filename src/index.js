b7b543d42f185d28669560274ed0e155c5bb4d0a
kaiser-3007 <sddrthpkhrna@gmail.com>
2020-06-02 23:27:30 +0530

    Divided into two pages, all scenes and single scene

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
 import styles from './styles.scss';
 import Scene from './components/Scene';
 import allScenes from './configs/allScenesConfig';
 import singleScene from './configs/singleSceneConfig';
 
-const App = () => (
+const AllScenes = () => (
   <div>
     <h1 className={styles.heading}>React - ThreeJS Experiments</h1>
+    <div className={styles.pages}>
+      <Link className={styles.page} to="/"><div>All Examples</div></Link>
+      <Link className={styles.page} to="/onescene"><div>Single Scene</div></Link>
+    </div>
     <div className={styles.examples}>
-      {/* {allScenes.map((props) => <Scene {...props} />)} */}
+      {allScenes.map((props, index) => <Scene key={index} {...props} />)}
+    </div>
+  </div>
+)
+
+const SingleScene = () => (
+  <div>
+    <h1 className={styles.heading}>React - ThreeJS Experiments</h1>
+    <div className={styles.pages}>
+      <Link className={styles.page} to="/"><div>All Examples</div></Link>
+      <Link className={styles.page} to="/onescene"><div>Single Scene</div></Link>
+    </div>
+    <div className={styles.examples}>
       <Scene {...singleScene} />
     </div>
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routing = () => (
+  <Router>
+    <Route exact path="/">
+      <AllScenes />
+    </Route>
+    <Route exact path="/onescene">
+      <SingleScene />
+    </Route>
+  </Router>
+)
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
